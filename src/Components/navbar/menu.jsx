4feb188ac0bcfa4fbaf74ab0bcbedb76e997d449
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2023-10-08 22:56:48 +0530

    pop up menu added

--- src/Components/navbar/menu.jsx
+++ src/Components/navbar/menu.jsx
@@ -1,9 +1,15 @@
+// PopupMenu.js
 import React from "react";
+import "./menu.css";
 
 const PopupMenu = ({ isOpen, onClose }) => {
   return (
-    <div className={`menu ${isOpen ? "open" : ""}`}>
-      <p>yo</p>
+    <div className={`popup-menu ${isOpen ? "open" : ""}`}>
+      <a href="/">Wishlist</a>
+      <a href="/">Bag</a>
+      <a href="/">Saved address</a>
+      <a href="/">About product</a>
+      <a href="/">Review</a>
       <button onClick={onClose}>Close</button>
     </div>
   );
--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,27 +1,32 @@
 import "./navbar.css";
-// import { useState } from "react";
-// import PopupMenu from "./menu";
+import { useState } from "react";
+import PopupMenu from "./menu";
 import bag from "../../Icons/bag.png";
 import logo from "../../Icons/logo.png";
 import menu from "../../Icons/menu1.png";
 function Navbar() {
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen);
-  // };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-  // const closeMenu = () => {
-  //   setIsMenuOpen(false);
-  // };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
     <div className="navbar">
-      {/* <span class="material-symbols-outlined">phone_iphone</span> */}
       <img src={logo} height={24} width={24} alt="product" />
       <p className="title">Balaji Covers</p>
       <img src={bag} height={24} width={24} alt="product" />
-      <img src={menu} height={24} width={36} alt="product" />
-      {/* <span class="material-symbols-outlined">menu</span> */}
+      <img
+        src={menu}
+        height={24}
+        width={36}
+        alt="product"
+        onClick={toggleMenu}
+      />
+      <PopupMenu isOpen={isMenuOpen} onClose={closeMenu} />
     </div>
   );
 }
