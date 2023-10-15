3938dcfd720b7e8c5840c980f1a2381d347f1c63
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2023-10-15 11:40:50 +0530

    minor update

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <img src={logo} height={24} width={24} alt="product" />
-      <p className="title">Balaji Covers</p>
+      <p className="title">Balaji Cover</p>
       <img src={bag} height={24} width={24} alt="product" />
       <img
         src={menu}
